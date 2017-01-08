ad60d94403f79d13853e06958878534600a1eca8
sarahama <sam8bz@virginia.edu>
2017-01-07 19:27:06 -0500

    Initial Commit

---
+++ moveMingo3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moveMingo3
+//
+//  Created by Sarah MacAdam on 1/7/17.
+//  Copyright © 2017 Sarah MacAdam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ moveMingo3Tests/moveMingo3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  moveMingo3Tests.swift
+//  moveMingo3Tests
+//
+//  Created by Sarah MacAdam on 1/7/17.
+//  Copyright © 2017 Sarah MacAdam. All rights reserved.
+//
+
+import XCTest
+@testable import moveMingo3
+
+class moveMingo3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
